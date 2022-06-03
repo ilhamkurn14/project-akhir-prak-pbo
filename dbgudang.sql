598fb2cc196a55afd54c03a40aed9a9d24a8254d
ILHAM KURNIAWAN ADI <ilhmkurnia09@gmail.com>
2022-06-03 20:38:51 +0700

    first commit

---
+++ dbgudang.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jun 2022 pada 15.34
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbgudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `harga` bigint(20) NOT NULL,
+  `gudang` varchar(100) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `satuan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `harga`, `gudang`, `stok`, `satuan`) VALUES
+(1, 'Beras Kitchen', 450000, 'Analog Mrican', 1, 'Karung'),
+(2, 'Gula', 9000, 'Analog Mrican', 20, 'Kg'),
+(3, 'Susu UHT Diamond', 15000, 'Analog Mrican', 30, 'Box'),
+(4, 'Susu Freshmilk Chimory', 15000, 'Analog Mrican', 50, 'Box'),
+(5, 'Beans Kopi Arabika', 180000, 'Analog Mrican', 5, 'Kg'),
+(6, 'Beans Kopi HB 50:50', 160000, 'Analog Mrican', 10, 'Kg'),
+(7, 'Sirup Denali Caramel', 100000, 'Analog Godean', 10, 'Pcs'),
+(8, 'Sirup Denali Rum', 100000, 'Analog Godean', 10, 'Pcs'),
+(9, 'Sirup Denali Hazelnut', 100000, 'Analog Godean', 10, 'Pcs'),
+(10, 'Sirup Denali Peach', 100000, 'Analog Godean', 10, 'Pcs'),
+(11, 'Sirup Denali Vanilla', 100000, 'Analog Godean', 10, 'Pcs');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'analogcoffeeyk', 'analogcoffeeyk', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
